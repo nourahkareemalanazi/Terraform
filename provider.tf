e306d87f47444f65494bfd6fa9e6dcd595542b53
nourah alanazi <106020183+nourahkareemalanazi@users.noreply.github.com>
2022-09-18 15:21:29 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
 region = "us-east-1"
-access_key = "AKIAWRYVSNSQCQREYXO2"
-secret_key = "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"
+access_key = "AKIA5AUJDUZNJDQCUMX7"
+secret_key = "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"
 }
