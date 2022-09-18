259ae4b8062377c101810a704bf7ed1fdc376340
nourah alanazi <106020183+nourahkareemalanazi@users.noreply.github.com>
2022-09-19 00:42:15 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,18 @@
-provider "aws"{
-region = "us-east-1"
-access_key = "AccesskeyID"
-secret_key = "Secretaccesskey"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+
+  access_key = "AccesskeyID"
+  secret_key = "Secretaccesskey"
+  region = "us-east-1"
+
 }
